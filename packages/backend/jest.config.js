bce0f67d9507b683a0aed8563b260784dcd76313
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2022-01-01 16:00:02 -0300

    (backend) bootstrap basic NestJS app

--- packages/backend/jest.config.js
+++
@@ -1,9 +0,0 @@
-const { name } = require('./package.json');
-
-module.exports = {
-  displayName: name,
-  name,
-  preset: 'ts-jest',
-  rootDir: '../../',
-  testPathIgnorePatterns: ['__tests__/utils/*.ts'],
-};
